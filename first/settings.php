e4ae169175594eb4f3d8e316ce6f15f0f1996b9f
EldinSalkovic <eldinsalkoviic@gmail.com>
2015-03-03 17:00:46 +0100

    First commit

---
+++ first/settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="Spine and Sports Physiatrists 183 North Addison Avenue, Suite 210 Elmhurst, Illinois 60126"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=6308344540;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
